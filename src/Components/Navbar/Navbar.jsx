f68fe1995f2caaa03f560939869f21aa94b7c4e5
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-03-29 18:40:51 +0500

    DevSphere IT Company

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,65 @@
-import React from "react";
-import './Navbar.css'
+// import React from "react";
+// import './Navbar.css'
+// import Insta from './insta.png'
+// import Telegram from './telegram.png'
+
+// function Navbar() {
+//     return(
+//         <nav>
+//             <div className="Center-Nav">
+//                 <p>DevSphere</p>
+//                 <div className="Nav-ul-link-center">
+//                     <div className="Nav-ul-link-width">
+//                         <div className="nav-ul-links">
+//                             <ul><link rel="stylesheet" href="" />Home</ul>
+//                             <ul><link rel="stylesheet" href="" />About</ul>
+//                             <ul><link rel="stylesheet" href="" />Works</ul>
+//                             <ul><link rel="stylesheet" href="" />Blogs</ul>
+//                             <ul><link rel="stylesheet" href="" />Contacts</ul>
+//                             <a href="https://www.instagram.com/devsphere.uz/"><img width={"40"} src={Insta} alt="" /></a>
+//                             <a href="https://t.me/JahongirFox"><img width={"40"} src={Telegram} alt="" /></a>
+//                         </div>
+//                     </div>    
+//                 </div>
+//             </div>
+//         </nav>
+//     )
+// }
+
+// export default Navbar
+
+import React, { useState } from "react";
+import "./Navbar.css";
+import Insta from "./insta.png";
+import Telegram from "./telegram.png";
 
 function Navbar() {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
         <nav>
             <div className="Center-Nav">
-                <p>DevSphere</p>
-                <div className="Nav-ul-link-center">
+                <p className="logo">DevSphere</p>
+                {/* Hamburger Button */}
+                <div className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+                    ☰
+                </div>
+                <div className={`Nav-ul-link-center ${isOpen ? "active" : ""}`}>
                     <div className="Nav-ul-link-width">
                         <div className="nav-ul-links">
-                            <ul><link rel="stylesheet" href="" />Home</ul>
-                            <ul><link rel="stylesheet" href="" />About</ul>
-                            <ul><link rel="stylesheet" href="" />Works</ul>
-                            <ul><link rel="stylesheet" href="" />Blogs</ul>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Works</a></li>
+                                <li><a href="#">Blogs</a></li>
+                                <li><a href="#">Contacts</a></li>
+                            </ul>
                         </div>
                     </div>    
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
