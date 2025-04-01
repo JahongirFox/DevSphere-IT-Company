706e722892fc2312c4e3a8b275c533b23eb0e3fc
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-04-01 17:12:45 +0500

    DevSphere IT Company bugs is fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Insta from "./insta.png";
 import Telegram from "./telegram.png";
-import Telephone from './telephone.png'
+import Telephone from "./telephone.png";
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -10,7 +11,7 @@ function Navbar() {
     return (
         <nav>
             <div className="Center-Nav">
-                <a href="/"><p className="logo">DevSphere</p></a>
+                <Link to="/"><p className="logo">DevSphere</p></Link>
                 {/* Hamburger Button */}
                 <div className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
                     ☰
@@ -19,23 +20,22 @@ function Navbar() {
                     <div className="Nav-ul-link-width">
                         <div className="nav-ul-links">
                             <ul>
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/about">About</a></li>
-                                {/* <li><a href="#">Blogs</a></li> */}
-                                <li><a href="#">Contacts</a></li>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/contact">Contacts</Link></li>
                             </ul>
                         </div>
                     </div>    
                 </div>
                 <div className="Images">
-                    <a href="https://www.instagram.com/devsphere.uz/">
+                    <a href="https://www.instagram.com/devsphere.uz/" target="_blank" rel="noopener noreferrer">
                         <img width="40" src={Insta} alt="Instagram" />
                     </a>
-                    <a href="https://t.me/JahongirFox">
+                    <a href="https://t.me/JahongirFox" target="_blank" rel="noopener noreferrer">
                         <img width="40" src={Telegram} alt="Telegram" />
                     </a>
                     <a href="tel:+998908257503">
-                        <img width="40" src={Telephone} alt="Telegram" />
+                        <img width="40" src={Telephone} alt="Telephone" />
                     </a>
                 </div>
             </div>
--- src/Pages/About/About1/About1.jsx
+++ src/Pages/About/About1/About1.jsx
@@ -4,7 +4,9 @@ import './About1.css'
 function About1() {
     return(
         <div className="About1">
-
+            <div className="About1-text">
+                <p>At DevSphere, we empower businesses <br /> and startups by crafting dynamic websites.<br /> Our tailored solutions drive results, elevate user <br /> experiences, and maximize profitability.</p>
+            </div>
         </div>
     )
 }
