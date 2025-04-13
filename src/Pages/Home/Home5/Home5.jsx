e850cf4da440268ef2e46484522e404742ce1b72
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-04-13 12:02:58 +0500

    DevSphere IT Company

---
+++ src/Pages/Home/Home5/Home5.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Home5.css';
+
+function Home5() {
+    return (
+        <div className="Home5">
+            <div className="home5-content">
+                <h2>Есть идея для проекта?</h2>
+                <button>Давайте обсудим</button>
+                <div className="home5-features">
+                    <span>Инструменты с ИИ-автоматизацией</span>
+                    <span>SEO-оптимизированные сайты</span>
+                    <span>Масштабируемые мобильные приложения</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home5;
--- src/Pages/Home/Main/Main.jsx
+++ src/Pages/Home/Main/Main.jsx
@@ -3,6 +3,7 @@ import Home1 from '../Home1/Home1'
 import Home2 from "../Home2/Home2";
 import Home3 from "../Home3/Home3";
 import Home4 from "../Home4/Home4";
+import Home5 from "../Home5/Home5";
 
 function Main() {
     return(
@@ -11,6 +12,7 @@ function Main() {
             <Home2/>
             <Home3/>
             <Home4/>
+            <Home5/>
         </>
     )
 }
