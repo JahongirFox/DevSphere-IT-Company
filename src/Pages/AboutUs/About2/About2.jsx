e9e9a2f305e08facbcc65687ac515a83c7cc5e4f
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-04-13 20:30:05 +0500

    DevSphere

--- src/Pages/AboutUs/About2/About2.jsx
+++ src/Pages/AboutUs/About2/About2.jsx
@@ -3,7 +3,7 @@ import './About2.css';
 import Il from '../../../assets/I.png';
 import umar from '../../../assets/umar.jpg';
 import saloh from '../../../assets/saloh.jpg';
-import eldos from '../../../assets/eldos.jpg'
+import eldos from '../../../assets/eldos.jpg';
 
 function About2() {
     return (
@@ -11,20 +11,35 @@ function About2() {
             <p className="title">Наша команда</p>
             <div className="team-cards">
                 <div className="card">
-                    <img src={Il} alt="Жахонгир Рахматжонов" />
-                    <p>Жахонгир <br /> Рахматжонов</p>
+                    <a href="https://www.instagram.com/raxmatjonoww/">
+                        <img src={Il} alt="Жахонгир Рахматжонов" />
+                        <p>Жахонгир <br /> Рахматжонов</p>
+                        <h4 className="position">Full-Stack Developer</h4>
+                    </a>
                 </div>
+
                 <div className="card">
-                    <img src={umar} alt="Умаржон Зохиджонов" />
-                    <p>Умаржон<br /> Зохиджонов</p>
+                    <a href="https://www.instagram.com/the.dosik55/">
+                        <img src={eldos} alt="Елдос Ержанов" />
+                        <p>Елдос <br /> Ержанов</p>
+                        <h4 className="position">Full-Stack Developer</h4>
+                    </a>
                 </div>
+
                 <div className="card">
-                    <img src={saloh} alt="Иброхимов Салохиддин" />
-                    <p>Иброхимов <br /> Салохиддин</p>
+                    <a href="https://www.instagram.com/z.u.b_06/">
+                        <img src={umar} alt="Умаржон Зохиджонов" />
+                        <p>Умаржон <br /> Зохиджонов</p>
+                        <h4 className="position">Web-Designer</h4>
+                    </a>
                 </div>
+
                 <div className="card">
-                    <img src={eldos} alt="Елдос Ержанов" />
-                    <p>Елдос <br /> Ержанов</p>
+                    <a href="https://www.instagram.com/_ibrkhmv___/">
+                        <img src={saloh} alt="Иброхимов Салохиддин" />
+                        <p>Иброхимов <br /> Салохиддин</p>
+                        <h4 className="position">Frontend Developer</h4>
+                    </a>
                 </div>
             </div>
         </div>
